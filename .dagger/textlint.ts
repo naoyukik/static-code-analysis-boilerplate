01b0f264d564681573b0ca609c38d70efa0479c1
Kodama Naoyuki <mmaga@dstribe.net>
2023-01-12 20:53:44 +0900

    fix: Add files you forgot to commit

---
+++ .dagger/textlint.ts
@@ -0,0 +1,22 @@
+import Client, { connect } from '@dagger.io/dagger';
+
+// initialize Dagger client
+connect(async (client: Client) => {
+  const source = client.host().directory('.', { exclude: ['node_modules/'] });
+  const node = client.container().from('bitnami/node:18')
+    .withExec(['corepack', 'enable', 'pnpm'])
+    .withExec(['corepack', 'prepare', 'pnpm@latest', '--activate']);
+
+  // textlint
+  const runner = client
+    .container({ id: node })
+    .withMountedDirectory('/app', source)
+    .withExec(['pnpm', 'install'])
+    .withExec(['pnpm', 'run', 'textlint']);
+
+  // execute
+  const textlintResult = await runner.stdout();
+
+  // print output
+  console.log(textlintResult);
+});
