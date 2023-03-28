20aacfd0e7d010ef69186c2b28b61df3bf9ae3aa
Kodama Naoyuki <mmaga@dstribe.net>
2023-03-28 20:47:19 +0900

    feat: Add "dagger:textlint:reviewdog" command

--- .dagger/textlint-with-reviewdog.ts
+++ .dagger/textlint-with-reviewdog.ts
@@ -2,20 +2,20 @@ import Client, { connect } from '@dagger.io/dagger';
 
 // initialize Dagger client
 connect(async (client: Client) => {
-  // get Node image
-  // get Node version
-  // const node = client.container().from('bitnami/node:18').withExec(['node', '-v']);
   const source = client.host().directory('.', { exclude: ['node_modules/'] });
   const node = client.container().from('bitnami/node:18')
     .withExec(['corepack', 'enable', 'pnpm'])
-    .withExec(['corepack', 'prepare', 'pnpm@latest', '--activate']);
+    .withExec(['corepack', 'prepare', 'pnpm@latest', '--activate'])
+    .withExec(['apt-get', 'update'])
+    .withExec(['apt-get', 'install', 'git'])
+    .withExec(['curl', '-sfLO', 'https://raw.githubusercontent.com/reviewdog/reviewdog/master/install.sh'])
+    .withExec(['sh', 'install.sh']);
 
   // textlint
-  const runner = client
-    .container({ id: node })
+  const runner = node
     .withMountedDirectory('/app', source)
     .withExec(['pnpm', 'install'])
-    .withExec(['pnpm', 'run', 'textlint']);
+    .withExec(['./bin/reviewdog', '-conf=./.reviewdog.yml', '-runners=textlint', '-diff=/usr/bin/git diff main']);
 
   // execute
   const textlintResult = await runner.stdout();
