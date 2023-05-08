5d3f75b7f20a18d463238559768427af67dd237f
Kodama Naoyuki <mmaga@dstribe.net>
2023-05-08 22:02:20 +0900

    fix: Changed environment variables for reviewdog to be called on command line

--- .dagger/textlint-with-reviewdog.ts
+++ .dagger/textlint-with-reviewdog.ts
@@ -4,7 +4,8 @@ import { Secret } from '@dagger.io/dagger/dist/api/client.gen'
 // initialize Dagger client
 connect(async (client: Client) => {
   // read secret from host variable
-  const secret: Secret = client.setSecret("gh-secret", <string>process.env["GH_SECRET"])
+  // const secret: Secret = client.setSecret("gh-secret", <string>process.env["GH_SECRET"])
+  const secret: Secret = client.setSecret("reviewdog-secret", <string>process.env["REVIEWDOG_GITHUB_API_TOKEN"])
 
   const source = client.host().directory('.', { exclude: ['node_modules/'] });
   const node = client.container().from('bitnami/node:18')
@@ -19,11 +20,11 @@ connect(async (client: Client) => {
   const runner = node
     .withMountedDirectory('/app', source)
     .withExec(['pnpm', 'install'])
-    .withSecretVariable('GITHUB_API_TOKEN', secret)
-    // .withExec(['echo', <string>process.env["GH_SECRET"]])
+    .withSecretVariable('GITHUB_TOKEN', secret)
+    .withExec(['echo', <string>process.env["REVIEWDOG_GITHUB_API_TOKEN"]])
     // .withExec(['printenv'])
     // .withExec(['REVIEWDOG_GITHUB_API_TOKEN=' + <string>process.env["REVIEWDOG_GITHUB_API_TOKEN"], './bin/reviewdog', '-conf=./.reviewdog.yml', '-runners=textlint_dagger', '-reporter=github-check', '-filter-mode=nofilter']);
-    .withExec(["REVIEWDOG_GITHUB_API_TOKEN=$GITHUB_API_TOKEN", './bin/reviewdog', '-conf=./.reviewdog.yml', '-runners=textlint', '-reporter=github-check', '-filter-mode=nofilter']);
+    // .withExec(["REVIEWDOG_GITHUB_API_TOKEN=$GITHUB_TOKEN", './bin/reviewdog', '-conf=./.reviewdog.yml', '-runners=textlint', '-reporter=github-check', '-filter-mode=nofilter']);
     // .withExec(['./bin/reviewdog', '-conf=./.reviewdog.yml', '-runners=textlint', '-diff=/usr/bin/git diff main']);
 
   // execute
