6ce6871c0c723ef9f51b8105444f0ea0857f030d
Kodama Naoyuki <mmaga@dstribe.net>
2024-03-02 22:24:32 +0900

    feat: add fixup check

--- dangerfile.ts
+++ dangerfile.ts
@@ -1,6 +1,7 @@
-import { danger, warn } from 'danger';
+import {danger, fail} from 'danger';
 
-// No PR is too small to include a description of why you made a change
-if (danger.github.pr.body.length < 10) {
-  warn('Please include a description of your PR changes.');
-}
+danger.git.commits.forEach((commit) => {
+  if (commit.message.startsWith('fixup!')) {
+    fail('Commit message contains "fixup".');
+  }
+});
