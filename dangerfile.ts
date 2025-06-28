e1f1d714b7ebace08e357c2bd00556db1067a272
Kodama Naoyuki <mmaga@dstribe.net>
2025-06-28 20:41:34 +0900

    refactor: modularize checks in dangerfile.ts

--- dangerfile.ts
+++ dangerfile.ts
@@ -1,7 +1,23 @@
 import {danger, fail} from 'danger';
 
-danger.git.commits.forEach((commit) => {
-  if (commit.message.startsWith('fixup!')) {
-    fail('Commit message contains "fixup".');
-  }
-});
+/**
+ * Run all checks
+ */
+function runChecks() {
+  checkCommitMessages();
+  // Additional checks can be added here in the future
+}
+
+/**
+ * Check if any commit message starts with 'fixup!'
+ */
+function checkCommitMessages() {
+  danger.git.commits.forEach((commit) => {
+    if (commit.message.startsWith('fixup!')) {
+      fail('Commit message contains "fixup".');
+    }
+  });
+}
+
+// Execute all checks
+runChecks();
